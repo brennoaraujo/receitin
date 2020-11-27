f0e9fd19aae5596d4eae726c31e5a91c8938082d
brennomeneses <brenno.meneses@tecpuc.com.br>
2020-11-27 17:22:12 -0300

    Modelo base da Aplicação

---
+++ js/script.js
@@ -0,0 +1,37 @@
+ $(document).ready(function () {
+ 	var count = 0;
+ 	var livro = [];
+ 	var idd = 0;
+ 	$("#paco").click(function(){
+ 		var val = "";
+ 		val += "º: <input class='form-control' placeholder='Passo número "+(count+1)+"' id='inpt"+count+"'></input><br>";
+ 		count++;
+ 		$("#etapas").append(count+val);
+ 	});
+ 	$("#sub").click(function(){
+ 		var n = []
+ 		idd++;
+ 		for (var i = 0; i < count; i++) {
+ 			var k = $("#inpt"+i).val();
+ 			n.push(k);
+ 		}
+ 		var pgDesci = $("#desc").val();
+ 		var receita = {
+ 			id: idd, //Inteiro
+ 			descri: pgDesci, //String
+ 			etapa: n //Array (de Strings)
+ 		};
+ 		livro.push(receita);
+ 		mostrarLivro("#livros",livro);
+ 	});
+ });
+
+function mostrarLivro(espaco, array){
+	var contador = 0;
+ 	for (var i = array.length-1; i >= 0; i--) {
+ 		for (var x = array[i].etapa.length - 1; x >= 0; x--) {
+ 			contador++
+ 			$(espaco).append(contador+"º: "+array[i].etapa[x]+"<br>");
+ 		}
+ 	}
+}
